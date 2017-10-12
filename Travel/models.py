d25c2acf20e0646fcd117338a255832233670d08
Mr. Ramzeg <mr.ramzeng@gmail.com>
2017-10-12 23:16:27 +0300

    correct base.html

--- Travel/models.py
+++ Travel/models.py
@@ -5,23 +5,6 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-from django.db.models.signals import post_save
-
-from django.dispatch import receiver
-# Здесь происходит создание моделей для сайта.
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-
-@receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
-
 class Manager(models.Model): # Модель менеджера
 	user = models.OneToOneField(User, on_delete = models.CASCADE) # Индивидуальный идентификатор пользователя 
 	surname = models.CharField("Фамилия", max_length = 20) # Фамилия 
@@ -71,7 +54,7 @@ class Tour(models.Model):
 		return unicode(self.name) + ' ' + unicode(self.city) + " " + unicode(self.date) + ', ' + unicode(self.days) + " дней, " + unicode(self.price) + "руб."
 
 class Tourist(models.Model): # Модель пользователя сайта (туриста)
-	user = models.OneToOneField(User, on_delete = models.CASCADE) # Индивидуальный идентификатор пользователя
+	user = models.ForeignKey(User, on_delete = models.CASCADE) # Индивидуальный идентификатор пользователя
 	surname = models.CharField("Фамилия", max_length = 20) # Фамилия 
 	name = models.CharField("Имя", max_length = 20) # Имя 
 	patronymic = models.CharField("Отчество", max_length = 20) # Отчество
--- Travel/views.py
+++ Travel/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render, redirect
 
 from . models import Manager, Tourist, Country, City, Tour, Region
 
-from datetime import datetime, timedelta, date
-
 from django.contrib.auth import login, authenticate
-
 from Travel.forms import SignUpForm
 
 def index(request):
@@ -25,17 +22,15 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.birth_date = form.cleaned_data.get('birth_date')
-            user.save()
+            form.save()
+            username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
+            user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('/Travel')
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form, 'signup': True, 'login': True})
+    return render(request, 'signup.html', {'form': form})
 
 def tour(request, tour_id = None, region_id = None):
     tour = Tour.objects.all()
